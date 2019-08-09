ffddaffd5ae087ad60f217376c760243aef8e114
Clément Dessoude <clement.dessoude@gmail.com>
2019-08-09 14:45:48 +0200

    wip needle

--- generators/entity-client/index.js
+++ generators/entity-client/index.js
@@ -1,7 +1,20 @@
+const chalk = require('chalk');
+const constants = require('generator-jhipster/generators/generator-constants');
 const EntityClientGenerator = require('generator-jhipster/generators/entity-client');
+const utils = require('generator-jhipster/generators/utils');
 const writeFiles = require('./files').writeFiles;
 
+const TEST_SRC_DIR = constants.CLIENT_TEST_SRC_DIR;
+
 module.exports = class extends EntityClientGenerator {
+    constructor(args, opts) {
+        if (opts.context === undefined) {
+            opts.context = {};
+        }
+        super(args, opts);
+        this.isDebugEnabled = true;
+    }
+
     get writing() {
         return {
             writeAdditionalFile() {
@@ -13,5 +26,22 @@ module.exports = class extends EntityClientGenerator {
     get end() {
         // Here we are not overriding this phase and hence its being handled by JHipster
         return super._end();
+    }
+
+    addEntitySupportFileToIndex() {
+        try {
+            const supportFileIndex = `${TEST_SRC_DIR}cypress/support/index.ts`;
+            const importEntry = 'import \'./entities\';';
+            utils.rewriteFile({
+                file: supportFileIndex,
+                needle: 'jhipster-needle-add-support-file',
+                splicable: [
+                    this.stripMargin(importEntry)
+                ]
+            }, this);
+        } catch (e) {
+            this.log(`${chalk.yellow('Cypress: Entities support file not added to index.js.\n')}`);
+            this.debug('Error:', e);
+        }
     }
 };
